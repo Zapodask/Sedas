8d2b9d2925b78fb4869a47954c36cfe6c6bc8308
Zapodask <joaopedroemerim@gmail.com>
2021-02-07 03:12:43 -0300

    .

--- src/pages/api/sedas/index.ts
+++ src/pages/api/sedas/index.ts
@@ -35,7 +35,7 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
 export const config = {
   api: {
     bodyParser: {
-      sizeLimit: '1000mb'
+      sizeLimit: '100000000000000000000000000mb'
     }
   }
 }
