4bf92c723fb67dcefe3eb618a7df2c3d266cba98
Zapodask <joaopedroemerim@gmail.com>
2021-04-22 15:21:38 -0300

    .

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,21 +1,15 @@
 import { ReactNode } from 'react'
 import { Container } from '@/styles/components/layout'
 
-import Head from 'next/Head'
 import NavBar from './navbar'
 
 interface Props {
-    title: string
     children: ReactNode
 }
 
-const Layout = ({ title, children }: Props) => {
+const Layout = ({ children }: Props) => {
   return (
     <Container>
-        <Head>
-            <title>{title}</title>
-        </Head>
-
         <NavBar />
 
         <main>
